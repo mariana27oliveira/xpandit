7d3f88f5b0214cf4870a133b6c545f9959bb1386
mariana27oliveira <122127683+mariana27oliveira@users.noreply.github.com>
2026-03-03 09:06:47 +0000

    Delete macros directory

--- macros/metadata.sql
+++
@@ -1,4 +0,0 @@
-{% macro add_metadata_columns() %}
-    current_timestamp() as created_at,
-    current_timestamp() as updated_at
-{% endmacro %}
--- macros/run_query.sql
+++
@@ -1,7 +0,0 @@
--- Takes a number of SQL queries separated by a ';' and runs them individually.
-
-{% macro run_queries(queries) -%}
-  {% for query in (queries | trim | trim(';')).split(';') -%}
-    {%- do run_query(query | trim) -%}
-  {%- endfor %}
-{%- endmacro %}
